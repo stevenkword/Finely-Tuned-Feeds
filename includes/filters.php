50a6ec80ad5e1c48dd35e067655a208c58404b71
Steven K Word <stevenword@gmail.com>
2015-03-10 13:38:22 -0700

    Clean up

--- includes/filters.php
+++ includes/filters.php
@@ -14,7 +14,7 @@ function debug_duck( $text ) {
 //add_filter( 'esc_xml', 'debug_duck' );
 
 /**
- * Replace Default Filters using esc_html with esc_xml
+ * Replace Default RSS Filters using esc_html with esc_xml
  */
 remove_filter( 'the_title_rss', 'esc_html' );
 add_filter( 'the_title_rss', 'esc_xml' );
@@ -22,29 +22,31 @@ add_filter( 'the_title_rss', 'esc_xml' );
 remove_filter( 'comment_text_rss', 'esc_html' );
 add_filter( 'comment_text_rss', 'esc_xml' );
 
-/* ---- Patches ---- */
-
-// Strip for XML
+/**
+ * Default filters for esc_html()
+ */
 add_filter( 'esc_xml', 'strip_for_xml' );
 //add_filter( 'esc_xml', 'patch_31190' );
 //add_filter( 'esc_xml', 'strip_non_xml_entities' );
 
-/**
- * 28816 - Wow! This fixes most things
- *
- * https://core.trac.wordpress.org/attachment/ticket/28816/order.patch
- */
-//remove_filter( 'the_title_rss', 'ent2ncr', 8 );
-//remove_filter( 'the_title_rss', 'esc_xml' );
-//add_filter( 'the_title_rss',    'esc_xml', 10 );
-//add_filter( 'the_title_rss',    'ent2ncr', 11 );
+	/**
+	 * 28816 - Wow! This fixes most things
+	 *
+	 * https://core.trac.wordpress.org/attachment/ticket/28816/order.patch
+	 */
+	add_filter( 'the_title_rss', 'esc_xml', 10 );
+	add_filter( 'the_title_rss', 'ent2ncr', 11 );
 
 /**
- * GUID
- * @pending 31080
+ * Other filters
  */
-remove_filter( 'the_guid', 'esc_url' );
-function html_escape_the_guid( $guid ) {
-	return esc_xml( $guid );
-}
-add_filter( 'the_guid', 'html_escape_the_guid' );
+
+	/**
+	 * GUID
+	 * @pending 31080
+	 */
+	remove_filter( 'the_guid', 'esc_url' );
+	function html_escape_the_guid( $guid ) {
+		return esc_xml( $guid );
+	}
+	add_filter( 'the_guid', 'html_escape_the_guid' );
