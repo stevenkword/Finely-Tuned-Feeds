e509236c43c238013c36bd122d9f77556091fda4
Steven K Word <stevenword@gmail.com>
2015-03-10 15:45:28 -0700

    reorganize

--- includes/filters.php
+++ includes/filters.php
@@ -7,43 +7,49 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 //add_filter( 'esc_xml', function() { return 'DEBUG'; } );
 
 /**
- * Replace Default RSS Filters using esc_html with esc_xml
+ * Escape the GUID with XML
+ * @ticket 31080
+ */
+remove_filter( 'the_guid', 'esc_url' );
+function html_escape_the_guid( $guid ) {
+	return esc_xml( $guid );
+}
+add_filter( 'the_guid', 'html_escape_the_guid' );
+
+/**
+ * Replace Core Title RSS Filters using `esc_html` with `esc_xml`
+ *
+ * This replaces the escaping method used by core with the
+ * `esc_xml()` method found in this plugin.
  */
 remove_filter( 'the_title_rss', 'esc_html' );
 add_filter( 'the_title_rss', 'esc_xml' );
+
+/**
+ * Replace Core Comments RSS Filters using `esc_html` with `esc_xml`
+ *
+ * This replaces the escaping method used by core with the
+ * `esc_xml()` method found in this plugin.
+ */
 remove_filter( 'comment_text_rss', 'esc_html' );
 add_filter( 'comment_text_rss', 'esc_xml' );
 
 /**
- * Default filters for esc_html()
+ * Apply patch filters
  */
 //add_filter( 'esc_xml', 'strip_for_xml' );
 //add_filter( 'esc_xml', 'patch_31190' );
 //add_filter( 'esc_xml', 'strip_non_xml_entities' );
 
-	/**
-	 * 28816 - Wow! This fixes most things
-	 *
-	 * https://core.trac.wordpress.org/attachment/ticket/28816/order.patch
-	 */
-	//add_filter( 'the_title_rss', 'esc_xml', 10 );
-	//add_filter( 'the_title_rss', 'ent2ncr', 11 );
-
-	/**
-	 * 9993 - Remove strip tags filter
-	 */
-	//remove_filter( 'the_title_rss', 'strip_tags' );
+/**
+ * 28816 - Wow! This fixes most things
+ *
+ * https://core.trac.wordpress.org/attachment/ticket/28816/order.patch
+ */
+//add_filter( 'the_title_rss', 'esc_xml', 10 );
+//add_filter( 'the_title_rss', 'ent2ncr', 11 );
 
 /**
- * Other filters
+ * 9993 - Remove strip tags filter
  */
-
-	/**
-	 * GUID
-	 * @pending 31080
-	 */
-	remove_filter( 'the_guid', 'esc_url' );
-	function html_escape_the_guid( $guid ) {
-		return esc_xml( $guid );
-	}
-	add_filter( 'the_guid', 'html_escape_the_guid' );
+//remove_filter( 'the_title_rss', 'strip_tags' );
