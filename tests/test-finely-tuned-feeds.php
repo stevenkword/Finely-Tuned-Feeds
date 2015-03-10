9e82159b4b77c2cf2d696e60c2a0d8ebf55e8dab
Steven K Word <stevenword@gmail.com>
2015-03-10 10:30:46 -0700

    tests for 9992 & 9993

--- tests/test-finely-tuned-feeds.php
+++ tests/test-finely-tuned-feeds.php
@@ -37,4 +37,30 @@ class Tests_Finely_Tuned_Feeds extends WP_UnitTestCase {
 		$this->assertEquals( $expected, $actual );
 	}
 
+	/**
+	 * Test potential encoding and formatting problems.
+	 * @return [type] [description]
+	 */
+	function test_htmlentities() {
+
+		// Title test for #9993, 9992
+		$title         = '& > test <';
+		$title_encoded = '&amp; &gt; test &lt;';
+
+		$this->assertEquals( $title, apply_filters( 'wp_title_rss', $title ) );
+		$this->assertEquals( $title_encoded, apply_filters( 'the_title_rss', $title ) );
+
+		$title         = '<test>This is a test</test>';
+		$title_encoded = '&lt;test&gt;This is a test&lt;/test&gt;';
+
+		$this->assertEquals( $title, apply_filters( 'wp_title_rss', $title ) );
+		$this->assertEquals( $title_encoded, apply_filters( 'the_title_rss', $title ) );
+
+		$title         = 'Use <h1> to <h6> for headings, <p> for paragraphy, but not formatting';
+		$title_encoded = 'Use &lt;h1&gt; to &lt;h6&gt; for headings, &lt;p&gt; for paragraphy, but not formatting';
+
+		$this->assertEquals( $title, apply_filters( 'wp_title_rss', $title ) );
+		$this->assertEquals( $title_encoded, apply_filters( 'the_title_rss', $title ) );
+	}
+
 }
